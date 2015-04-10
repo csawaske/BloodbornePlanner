125cb41f05b6fd77d8845591ee3669077767780a
csawaske <Connor@Connors-iMac.local>
2015-04-10 12:59:57 -0700

    ..

--- Bloodborne Planner/Bloodborne Planner/ViewController.swift
+++ Bloodborne Planner/Bloodborne Planner/ViewController.swift
@@ -8,41 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate{
+class ViewController: UIViewController, UIPickerViewDelegate, UITextFieldDelegate{
     
     var genders = ["male", "female"]
     var classes = ["Milquetoast", "Lone Survivor", "Troubled Childhood",
             "Violent Past", "Professional", "Military Veteran", "Noble Scion",
             "Cruel Fate", "Waste of Skin"]
-    var thisCharacter = Character(entryName: "", entryGender: "", charType: "")
+    var characters = [Character]()
 
     @IBOutlet weak var nameInput: UITextField!
     @IBOutlet weak var genderChoice: UIPickerView!
     @IBOutlet weak var classChoice: UIPickerView!
     @IBOutlet weak var summary: UILabel!
+    @IBOutlet weak var genderLabel: UITextField!
 
     
-    @IBAction func buttonPress(sender: AnyObject) {
+    @IBAction func pressedTheButton(sender: AnyObject) {
+        var selectedClass = classChoice.selectedRowInComponent(0)
         
-        switch classChoice.selectedRowInComponent(0) {
-        case 2:
-            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " with a " + classes[classChoice.selectedRowInComponent(0)].lowercaseString + " named " + nameInput.text
-        case 3:
-            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " with a " + classes[classChoice.selectedRowInComponent(0)].lowercaseString + " named " + nameInput.text
-        case 7:
-            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " with a " + classes[classChoice.selectedRowInComponent(0)].lowercaseString + " named " + nameInput.text
-        default:
-            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " " + classes[classChoice.selectedRowInComponent(0)].lowercaseString + " named " + nameInput.text
+        if (selectedClass == 2 || selectedClass == 3 || selectedClass == 7) {
+            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " with a " + classes[selectedClass].lowercaseString + " named " + nameInput.text
+        } else {
+            summary.text = "You have created a " + genders[genderChoice.selectedRowInComponent(0)].lowercaseString + " " + classes[selectedClass].lowercaseString + " named " + nameInput.text
         }
-        
-        
-
-        thisCharacter = Character(entryName: nameInput.text, entryGender: genders[genderChoice.selectedRowInComponent(0)], charType: classes[classChoice.selectedRowInComponent(0)])
-
+        characters.append(Character(entryName: nameInput.text, entryGender: genders[genderChoice.selectedRowInComponent(0)], charType: classes[selectedClass]))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        genderChoice.delegate = self
+        genderLabel.delegate = self
+        genderChoice.hidden = true
+        genderLabel.text = nil
+        summary.numberOfLines = 0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -56,22 +54,43 @@ class ViewController: UIViewController, UIPickerViewDelegate{
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView.tag == 0 {
+        switch pickerView.tag {
+        case 0:
             return classes.count
-        } else {
-        return genders.count
+        case 1:
+            return genders.count
+        case 2:
+            return characters.count
+        default:
+            return 0
         }
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String!{
-        if pickerView.tag == 0 {
+        switch pickerView.tag {
+        case 0:
             return classes[row]
-        } else {
+        case 1:
             return genders[row]
+        case 2:
+            return characters[row].name
+        default:
+            return ""
         }
     }
     
-
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        genderChoice.hidden = false
+        return false
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if (pickerView.tag == 1) {
+            genderLabel.text = genders[row]
+            genderChoice.hidden = true;
+        }
+    }
     
 }
 
